2257ccf12ef4007c21ee833858a5398ff9ea0c07
Manni <emmanuelmelgoza1@gmail.com>
2024-04-11 21:01:06 -0600

    Solved ALL Part2

--- problems/08-advanced-exponent.js
+++ problems/08-advanced-exponent.js
@@ -18,32 +18,45 @@ Remember that you don't need to do anything special to square a number, just
 calculate the value and multiply it by itself. So don't cheat and use
 exponentiation in your solution.
 
-Examples:
-
-advancedExponent(2, 0); // 1
-advancedExponent(2, 1); // 2
-advancedExponent(2, 2); // 4
-advancedExponent(2, 3); // 8
-advancedExponent(2, 4); // 16
-advancedExponent(2, 5); // 32
-advancedExponent(2, 6); // 64
-advancedExponent(2, 7); // 128
-advancedExponent(2, 8); // 256
-advancedExponent(2, 9); // 512
-advancedExponent(2, 10); // 1024
-advancedExponent(2, 11); // 2048
-advancedExponent(2, 12); // 4096
-
 For each of the examples above, figure out how many times your code should
 be recursively calling `advancedExponent`. Find a way to visually see how many
 times `advancedExponent` is being recursively called.
 ***********************************************************************/
 
-
 function advancedExponent(b, n) {
-  // Your code here 
-}
+  // Your code here
+  // BASE CASES
+  if (n === 0) return 1;
 
+  if (n === 1) return b;
+
+  // RECURSION CASES
+  if (n % 2 === 0) {
+    n /= 2
+    return b * advancedExponent(b, n - 1) * b * advancedExponent(b, n - 1);
+  }
+
+  if (n % 2 !== 0) {
+    //the n - 1 param decreases the n value by 1
+    return  b * advancedExponent(b, n - 1)
+  }
+  
+};
+
+console.log(advancedExponent(2, 0)); // 1
+console.log(advancedExponent(2, 1)); // 2
+console.log(advancedExponent(2, 2)); // 4
+console.log(advancedExponent(2, 3)); // 8
+console.log(advancedExponent(2, 4)); // 16
+console.log(advancedExponent(2, 5)); // 32
+console.log(advancedExponent(2, 6)); // 64
+console.log(advancedExponent(2, 7)); // 128
+console.log(advancedExponent(2, 8)); // 256
+console.log(advancedExponent(2, 9)); // 512
+console.log(advancedExponent(2, 10)); // 1024
+debugger
+console.log(advancedExponent(2, 11)); // 2048
+console.log(advancedExponent(2, 12)); // 4096
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
